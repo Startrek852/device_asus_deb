321865d88ab2188959a872b974f7a7f14e9109bd
Andrew Vance <andrew.j.vance@gmail.com>
2017-01-12 22:13:10 -0700

    Revert "deb: inherit common blobs from flo"
    This reverts commit 1e66e3f9fd3a9c87f82ba780a9c308111a78ddb2.
    
    Change-Id: I912182cd34dc138b2b86cf70e44df87d3f1fefd1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -32,25 +32,14 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract "$MY_DIR"/proprietary-blobs.txt "$SRC"
+# Copyright headers and guards
+write_headers
 
-"$MY_DIR"/setup-makefiles.sh
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-blobs.txt
+
+# Done
+write_footers
